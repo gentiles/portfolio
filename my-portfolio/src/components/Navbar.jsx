81dcc3222b06c12e7e823c842b964ec9900630ca
erwin <erwinmarin05@gmail.com>
2025-03-02 04:55:14 +0800

    done navabar

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,7 +1,25 @@
+import links from "../data";
 const Navbar = () => {
+  const link = links.map((item) => {
+    const { id, href, text } = item;
+    return (
+      <a
+        key={id}
+        href={href}
+        className="capitalize text-lg tracking-wide hover:text-emerald-600 duration-300"
+      >
+        {text}
+      </a>
+    );
+  });
   return (
     <nav className="bg-emerald-100">
-      <div>Navbar</div>
+      <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
+        <h2 className="text-3xl font-bold">
+          Web<span className="text-emerald-600">Dev</span>
+        </h2>
+        <div className="flex gap-x-3 ">{link}</div>
+      </div>
     </nav>
   );
 };
